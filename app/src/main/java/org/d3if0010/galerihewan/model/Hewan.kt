f0fdeb72c2f01e76863306a2ecd8d0afaa3b410f
luckysopyantoro <luckysopyantoro@gmail.com>
2021-05-03 03:28:59 +0700

    3.3.Mengambil data dari internet

--- app/src/main/java/org/d3if0010/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if0010/galerihewan/model/Hewan.kt
@@ -2,6 +2,6 @@ package org.d3if0010.galerihewan.model
 
 data class Hewan(
         val nama: String,
-        val jenisHewan: String,
-        val imageResId: Int
+        val namaLatin: String,
+        val imageId: Int
 )
